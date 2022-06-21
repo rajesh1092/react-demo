6eb5da1613c9ca9572818cc622b481ff08014ade
rajesh <rajesh.kansara@exhibytesolution.com>
2022-06-21 11:53:04 +0530

    stopwatch

---
+++ watch-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import StopWatch  from './watch/stopwatch/stopwatch'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StopWatch />
+    </div>
+  );
+}
+
+export default App;
